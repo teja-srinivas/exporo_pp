90afd50846324031d2764f9ea2b1c4fce5577f1c
André Gräßel <hello@spaceemotion.net>
2018-04-23 17:32:02 +0200

    Extract external dependencies in a vendor.js file

--- application/resources/views/layouts/app.blade.php
+++ application/resources/views/layouts/app.blade.php
@@ -11,6 +11,8 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
+    <script src="{{ mix('js/manifest.js') }}" defer></script>
+    <script src="{{ mix('js/vendor.js') }}" defer></script>
     <script src="{{ mix('js/app.js') }}" defer></script>
 
     <!-- Fonts -->
--- application/webpack.mix.js
+++ application/webpack.mix.js
@@ -12,4 +12,8 @@ let mix = require('laravel-mix');
  */
 
 mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+mix.sass('resources/assets/sass/app.scss', 'public/css')
+mix.extract([
+    'axios',
+    'jquery',
+]);
