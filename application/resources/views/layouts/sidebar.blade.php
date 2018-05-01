5c81b3e3bfb3f029df87e82cf16a4936f09fd378
André Gräßel <hello@spaceemotion.net>
2018-05-01 22:43:37 +0200

    Make main navigation and sidebar sticky

--- application/resources/views/layouts/sidebar.blade.php
+++ application/resources/views/layouts/sidebar.blade.php
@@ -4,23 +4,25 @@
     <div class="container">
         <div class="row">
             <div class="col-md-3">
-                @foreach($menu as $group)
-                    <h6 class="text-muted text-uppercase tracking-wide sidebar-header @unless($loop->first) mt-4 @endif">
-                        <span>
-                            {{ $group['title'] }}
+                <div class="sidebar">
+                    @foreach($menu as $group)
+                        <h6 class="text-muted text-uppercase tracking-wide sidebar-header @unless($loop->first) mt-4 @endif">
+                            <span>
+                                {{ $group['title'] }}
 
-                            @unless(empty($group['help'] ?? ''))
-                                <a href="{{ $group['help'] }}" class="small text-primary ml-2">Hilfe?</a>
-                            @endif
-                        </span>
-                    </h6>
+                                @unless(empty($group['help'] ?? ''))
+                                    <a href="{{ $group['help'] }}" class="small text-primary ml-2">Hilfe?</a>
+                                @endif
+                            </span>
+                        </h6>
 
-                    @foreach($group['links'] as $route => $link)
-                        <a href="{{ $route }}"
-                           class="sidebar-item @if(request()->routeIs($route))shadow-sm active @endif"
-                        >{{ $link }}</a>
+                        @foreach($group['links'] as $link)
+                            <a href="{{ $link['url'] }}"
+                               class="sidebar-item @if($link['isActive'] ?? false)shadow-sm active @endif"
+                            >{{ $link['title'] }}</a>
+                        @endforeach
                     @endforeach
-                @endforeach
+                </div>
             </div>
             <div class="col-md-9">
                 @yield('main-content')
