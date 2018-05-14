d6aa1cc8124cb1f0ec66388f2a2ca6dd518e0acd
André Gräßel <hello@spaceemotion.net>
2018-05-14 17:12:53 +0200

    Convert audit table into a full table (no columns)
    That way we let the browser decide on the column width and how to lay out the content.

--- application/resources/views/components/audit.blade.php
+++ application/resources/views/components/audit.blade.php
@@ -1,55 +1,61 @@
 @can('view audits')
-<div class="table-responsive-md shadow-sm mt-3">
-<table class="table table-sm small table-striped mb-0 bg-white">
-    <thead>
-    <tr class="border-bottom">
-        <th class="border-top-0">Name</th>
-        <th class="border-top-0">Änderungen</th>
-    </tr>
-    </thead>
-    <tbody>
-    @forelse($model->audits->load('user')->sortByDesc('created_at') as $audit)
+<div class="table-responsive shadow-sm mt-3">
+    <table class="table table-sm small table-striped mb-0 bg-white">
+        <thead>
         <tr>
-            <td width="130">
-                <strong class="mb-0">
-                    <a href="{{ route('users.show', $audit->user) }}">
-                        {{ $audit->user->first_name }}
-                        {{ $audit->user->last_name }}
-                    </a>
-                </strong>
-                <br>
-                <abbr title="{{ $audit->created_at }}">
-                    {{ $audit->created_at->diffForHumans() }}
-                </abbr>
-            </td>
-            <td>
+            <th class="border-top-0">Name</th>
+            <th class="border-top-0">Änderung</th>
+            <th class="border-top-0">Von</th>
+            <th class="border-top-0">In</th>
+        </tr>
+        </thead>
+        <tbody>
+        @forelse($model->audits->load('user')->sortByDesc('created_at') as $audit)
+            @php($_modifications = $audit->getModified())
+
+            <tr>
+                <td width="130" class="border-right" rowspan="{{ count($_modifications) }}">
+                    <strong class="mb-0">
+                        <a href="{{ route('users.show', $audit->user) }}">
+                            {{ $audit->user->first_name }}
+                            {{ $audit->user->last_name }}
+                        </a>
+                    </strong>
+                    <br>
+                    <abbr title="{{ $audit->created_at }}">
+                        {{ $audit->created_at->diffForHumans() }}
+                    </abbr>
+                </td>
                 @foreach($audit->getModified() as $title => $row)
-                    <div class="row">
-                        <div class="col-sm-4 col-lg-2 font-weight-bold">{{ $title }}</div>
-                        <div class="col-sm-4 col-lg-5">
-                            @if(is_bool($row['old']))
-                                {!! $row['old'] ? '✔' : '-' !!}
-                            @else
-                                {{ $row['old'] }}
-                            @endif
-                        </div>
-                        <div class="col-sm-4 col-lg-5">
-                            @if(is_bool($row['new']))
-                                {!! $row['new'] ? '✔' : '-' !!}
-                            @else
-                                {{ $row['new'] }}
-                            @endif
-                        </div>
-                    </div>
+                    @unless($loop->first)
+                        </tr><tr>
+                    @endif
+
+                    <td>
+                        <strong>{{ $title }}</strong>
+                    </td>
+                    <td>
+                        @if(is_bool($row['old']))
+                            {!! $row['old'] ? '✔' : '-' !!}
+                        @else
+                            {{ $row['old'] }}
+                        @endif
+                    </td>
+                    <td>
+                        @if(is_bool($row['new']))
+                            {!! $row['new'] ? '✔' : '-' !!}
+                        @else
+                            {{ $row['new'] }}
+                        @endif
+                    </td>
                 @endforeach
-            </td>
-        </tr>
-    @empty
-        <tr class="disabled">
-            <td colspan="2" class="text-center text-muted">Noch keine Änderungen vollzogen</td>
-        </tr>
-    @endforelse
-    </tbody>
-</table>
+            </tr>
+        @empty
+            <tr class="disabled">
+                <td colspan="2" class="text-center text-muted">Noch keine Änderungen vollzogen</td>
+            </tr>
+        @endforelse
+        </tbody>
+    </table>
 </div>
 @endcan
