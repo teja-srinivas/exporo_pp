b53d3959298141d2e06aaefbae56b7094d6b9124
André Gräßel <hello@spaceemotion.net>
2018-05-14 16:54:06 +0200

    Update model audit table
    We now support boolean display with UTF-8 checkmarks/crosses 🎉 as well as change the table to be bordered again for a clear(er) differentiation between entries

--- application/resources/views/components/audit.blade.php
+++ application/resources/views/components/audit.blade.php
@@ -1,22 +1,23 @@
 @can('view audits')
 <div class="table-responsive-md shadow-sm mt-3">
-<table class="table table-sm small table-borderless table-striped mb-0 bg-white">
+<table class="table table-sm small table-striped mb-0 bg-white">
     <thead>
     <tr class="border-bottom">
-        <th>Name</th>
-        <th>Änderungen</th>
+        <th class="border-top-0">Name</th>
+        <th class="border-top-0">Änderungen</th>
     </tr>
     </thead>
     <tbody>
     @forelse($model->audits->load('user')->sortByDesc('created_at') as $audit)
         <tr>
             <td width="130">
-                <h6 class="mb-0">
+                <strong class="mb-0">
                     <a href="{{ route('users.show', $audit->user) }}">
                         {{ $audit->user->first_name }}
                         {{ $audit->user->last_name }}
                     </a>
-                </h6>
+                </strong>
+                <br>
                 <abbr title="{{ $audit->created_at }}">
                     {{ $audit->created_at->diffForHumans() }}
                 </abbr>
@@ -25,8 +26,20 @@
                 @foreach($audit->getModified() as $title => $row)
                     <div class="row">
                         <div class="col-sm-4 col-lg-2 font-weight-bold">{{ $title }}</div>
-                        <div class="col-sm-4 col-lg-5">{{ $row['old'] }}</div>
-                        <div class="col-sm-4 col-lg-5">{{ $row['new'] }}</div>
+                        <div class="col-sm-4 col-lg-5">
+                            @if(is_bool($row['old']))
+                                {!! $row['old'] ? '✔' : '-' !!}
+                            @else
+                                {{ $row['old'] }}
+                            @endif
+                        </div>
+                        <div class="col-sm-4 col-lg-5">
+                            @if(is_bool($row['new']))
+                                {!! $row['new'] ? '✔' : '-' !!}
+                            @else
+                                {{ $row['new'] }}
+                            @endif
+                        </div>
                     </div>
                 @endforeach
             </td>
