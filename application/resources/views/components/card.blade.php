979f329714bf269ef817fee96069f6ae8f4a616b
André Gräßel <hello@spaceemotion.net>
2018-05-17 17:52:56 +0200

    Add info section support to our cards component

--- application/resources/views/components/card.blade.php
+++ application/resources/views/components/card.blade.php
@@ -1,22 +1,35 @@
 <div class="card shadow-sm border-0 accent-primary">
     <div class="card-body">
         {{-- Card Header --}}
-        @if(isset($title))
+        @if(isset($title) && !isset($info))
         <div class="card-title">
-            <h4 @if(isset($subtitle))class="mb-1"@endif>{{ $title }}</h4>
-            @if(isset($subtitle))
-                {{ $subtitle }}
-            @endif
+            <h5 @isset($subtitle)class="mb-1"@endisset>{{ $title }}</h5>
+            {{ $subtitle or '' }}
         </div>
         @endif
 
         {{-- Card Contents --}}
-        {{ $slot }}
+        @isset($info)
+            <div class="row">
+                <div class="col-lg-4 mb-3 mb-lg-0 small text-dark">
+                    @isset($title)
+                        <h5 class="card-title">{{ $title }}</h5>
+                    @endif
+
+                    {{ $info }}
+                </div>
+                <div class="col-lg-8">
+                    {{ $slot }}
+                </div>
+            </div>
+        @else
+            {{ $slot }}
+        @endisset
     </div>
 
-    @if(isset($footer))
+    @isset($footer)
     <div class="card-footer">
         {{ $footer }}
     </div>
-    @endif
+    @endisset
 </div>
