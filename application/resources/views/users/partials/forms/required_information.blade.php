bc28df407407c8d87c9b8b9b12991cd45136836c
André Gräßel <hello@spaceemotion.net>
2018-05-27 21:01:53 +0200

    Use the form builder for the user’s required_information

--- application/resources/views/users/partials/forms/required_information.blade.php
+++ application/resources/views/users/partials/forms/required_information.blade.php
@@ -1,35 +1,24 @@
-<div class="form-group row">
-    <label for="inputFirstName" class="col-sm-3 col-form-label">{{ __('First Name') }}:</label>
-    <div class="col-sm-9">
-        @include('components.form.input', [
-            'name' => 'first_name',
-            'autocomplete' => 'given-name',
-            'required' => true,
-            'default' => optional($user ?? null)->first_name,
-        ])
-    </div>
-</div>
-
-<div class="form-group row">
-    <label for="inputLastName" class="col-sm-3 col-form-label">{{ __('Last Name') }}:</label>
-    <div class="col-sm-9">
-        @include('components.form.input', [
-            'name' => 'last_name',
-            'autocomplete' => 'family-name',
-            'required' => true,
-            'default' => optional($user ?? null)->last_name,
-        ])
-    </div>
-</div>
-
-<div class="form-group row mb-0">
-    <label for="inputEmail" class="col-sm-3 col-form-label">{{ __('E-Mail Address') }}:</label>
-    <div class="col-sm-9">
-        @include('components.form.input', [
-            'name' => 'email',
-            'autocomplete' => 'email',
-            'required' => true,
-            'default' => optional($user ?? null)->email,
-        ])
-    </div>
-</div>
+@include('components.form.builder', ['inputs' => [
+    [
+        'name' => 'first_name',
+        'label' => __('First Name'),
+        'autocomplete' => 'given-name',
+        'required' => true,
+        'default' => optional($user ?? null)->first_name,
+    ],
+    [
+        'name' => 'last_name',
+        'label' => __('Last Name'),
+        'autocomplete' => 'family-name',
+        'required' => true,
+        'default' => optional($user ?? null)->last_name,
+    ],
+    [
+        'type' => 'email',
+        'name' => 'email',
+        'label' => __('E-Mail Address'),
+        'autocomplete' => 'email',
+        'required' => true,
+        'default' => optional($user ?? null)->email,
+    ],
+]])
