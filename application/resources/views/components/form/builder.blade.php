f23d90eb0534fc2b6265597a8b739465f248de98
André Gräßel <hello@spaceemotion.net>
2018-05-27 21:49:48 +0200

    Widen form fields (3 to 4 columns) in form builder

--- application/resources/views/components/form/builder.blade.php
+++ application/resources/views/components/form/builder.blade.php
@@ -4,10 +4,10 @@
     <div class="form-group row {{ $loop->last ? 'mb-0' : '' }}">
         <label
             for="input{{ studly_case($input['name']) }}"
-            class="col-sm-3 col-form-label"
+            class="col-sm-4 col-form-label"
         >{{ $input['label'] }}:</label>
 
-        <div class="col-sm-9 {{ $type === 'radio' ? 'pt-1' : '' }}">
+        <div class="col-sm-8 {{ $type === 'radio' ? 'pt-1' : '' }}">
             @includeFirst(["components.form.{$type}", 'components.form.input'], $input)
         </div>
     </div>
