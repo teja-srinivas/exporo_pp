ec205c2a76691fa181861a694437185617593824
André Gräßel <hello@spaceemotion.net>
2018-06-04 22:01:21 +0200

    Add label and input width options to form builder

--- application/resources/views/components/form/builder.blade.php
+++ application/resources/views/components/form/builder.blade.php
@@ -1,13 +1,15 @@
 @foreach($inputs as $input)
     @php($type = $input['type'] ?? 'text')
+    @php($colWidthLabel = $labelWidth ?? 4)
+    @php($colWidthInput = $inputWidth ?? 12 - $colWidthLabel)
 
     <div class="form-group row {{ $loop->last ? 'mb-0' : '' }}">
         <label
             for="input{{ studly_case($input['name']) }}"
-            class="col-sm-4 col-form-label"
+            class="col-sm-{{ $colWidthLabel }} col-form-label"
         >{{ $input['label'] }}:</label>
 
-        <div class="col-sm-8 {{ $type === 'radio' ? 'pt-1' : '' }}">
+        <div class="col-sm-{{ $colWidthInput }} {{ $type === 'radio' ? 'pt-1' : '' }}">
             @includeFirst(["components.form.{$type}", 'components.form.input'], $input)
         </div>
     </div>
