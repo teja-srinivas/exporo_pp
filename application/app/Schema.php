181964e55159234d904e65075ccd34b3cd08a4fe
André Gräßel <hello@spaceemotion.net>
2018-07-06 22:57:31 +0200

    Add formula parsing to schemas

--- application/app/Schema.php
+++ application/app/Schema.php
@@ -4,6 +4,9 @@ namespace App;
 
 use Cog\Laravel\Optimus\Traits\OptimusEncodedRouteKey;
 use Illuminate\Database\Eloquent\Model;
+use MathParser\Interpreting\Evaluator;
+use MathParser\Parsing\Nodes\Node;
+use MathParser\StdMathParser;
 use OwenIt\Auditing\Auditable;
 use OwenIt\Auditing\Contracts\Auditable as AuditableContract;
 
@@ -25,5 +28,24 @@ class Schema extends Model implements AuditableContract
     public function projects()
     {
         return $this->hasMany(Project::class, 'schema_id');
+    }
+
+    // TODO change this signature to match the correct calculation
+    public function calculate($firstVar, $secondVar)
+    {
+        // We cache the AST for faster lookups
+        $cacheKey = "schema.formulas.{$this->formula}";
+
+        /** @var Node $ast */
+        $ast = cache()->rememberForever($cacheKey, function () {
+            $parser = new StdMathParser();
+            return $parser->parse($this->formula);
+        });
+
+        // TODO parse the proper variables
+        return $ast->accept(new Evaluator([
+            'x' => $firstVar,
+            'y' => $secondVar,
+        ]));
     }
 }
