0a1d4b3fb5f1090d7cf876a660f197b5a02074f0
André Gräßel <hello@spaceemotion.net>
2018-07-22 14:00:25 +0200

    Add back Optimus for Investments and User models

--- application/app/Investment.php
+++ application/app/Investment.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Carbon\Carbon;
+use Cog\Laravel\Optimus\Traits\OptimusEncodedRouteKey;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -24,6 +25,7 @@ use OwenIt\Auditing\Contracts\Auditable as AuditableContract;
 class Investment extends Model implements AuditableContract
 {
     use Auditable;
+    use OptimusEncodedRouteKey;
 
     const MORPH_NAME = 'investment';
 
