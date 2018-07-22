627e458c8fa1d04618e65b687efb9bf4c7deace4
André Gräßel <hello@spaceemotion.net>
2018-07-22 15:30:29 +0200

    Add .editorconfig

--- application/webpack.mix.js
+++ application/webpack.mix.js
@@ -13,12 +13,12 @@ require('laravel-mix-purgecss');
  */
 
 mix.webpackConfig({
-    resolve: {
-        alias: {
-            // Use slim build to exclude the ajax stuff (we use axios)
-            jquery$: 'jquery/dist/jquery.slim.js',
-        },
+  resolve: {
+    alias: {
+      // Use slim build to exclude the ajax stuff (we use axios)
+      jquery$: 'jquery/dist/jquery.slim.js',
     },
+  },
 });
 
 mix.js('resources/assets/js/app.js', 'public/js')
@@ -26,30 +26,30 @@ mix.sass('resources/assets/sass/app.scss', 'public/css')
 mix.version();
 
 mix.purgeCss({
-    // Dropdowns and animated JS content
-    whitelist: ['show', 'fade', 'collapse', 'collapsing'],
+  // Dropdowns and animated JS content
+  whitelist: ['show', 'fade', 'collapse', 'collapsing'],
 
-    // We use badges for user role colors
-    whitelistPatterns: [/^badge-/],
+  // We use badges for user role colors
+  whitelistPatterns: [/^badge-/],
 });
 
 mix.extract([
-    'popper.js',
-    'bootstrap/js/src/dropdown',
-    'bootstrap/js/src/util',
-    'jquery',
+  'popper.js',
+  'bootstrap/js/src/dropdown',
+  'bootstrap/js/src/util',
+  'jquery',
 ]);
 
 // Allow custom bootstrap builds by using their source files
 // This enables the babel-loader to properly compile them
-Mix.listen('configReady', ({ module }) => {
-    module.rules.find(rule => {
-        if (rule.test.test && rule.test.test('test.js')) {
-            delete rule.exclude;
-            rule.include = [
-                __dirname + '/resources/assets/js/',
-                __dirname + '/node_modules/bootstrap/js/',
-            ];
-        }
-    });
+Mix.listen('configReady', ({module}) => {
+  module.rules.find(rule => {
+    if (rule.test.test && rule.test.test('test.js')) {
+      delete rule.exclude;
+      rule.include = [
+        __dirname + '/resources/assets/js/',
+        __dirname + '/node_modules/bootstrap/js/',
+      ];
+    }
+  });
 });
