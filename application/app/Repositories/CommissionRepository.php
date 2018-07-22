8e30a7947fc095fca6c62047fcc83e8a91c11a00
André Gräßel <hello@spaceemotion.net>
2018-07-22 23:45:25 +0200

    Remove irrelevant method from CommissionRepository

--- application/app/Repositories/CommissionRepository.php
+++ application/app/Repositories/CommissionRepository.php
@@ -3,14 +3,7 @@ declare(strict_types=1);
 
 namespace App\Repositories;
 
-use Illuminate\Support\Facades\DB;
-
 class CommissionRepository
 {
-    public function getNewestUpdatedAtDate()
-    {
-        return DB::table('projects')
-            ->orderBy('updated_at', 'desc')
-            ->first(['updated_at']);
-    }
+
 }
