f415aa82953ba9682550860e2d8f91c4f37bccca
André Gräßel <hello@spaceemotion.net>
2018-09-07 18:26:21 +0200

    Only show parts of the bill when we have content

--- application/resources/views/bills/bill.blade.php
+++ application/resources/views/bills/bill.blade.php
@@ -2,22 +2,24 @@
 
 @section('cover')
     @include('bills.cover')
-@endsection
 
-@section('projects')
     <h1 class="text-center mt-5"> Übersicht Provisionsgutschrift</h1>
-    <h3 class="mt-5 text-center">Eigenumsatz</h3>
-
-    @include('bills.projects')
-
 @endsection
 
+@unless($investments->isEmpty())
+@section('projects')
+    <h3 class="mt-5 text-center">Eigenumsatz</h3>
+    @include('bills.projects')
+@endsection
+@endunless
+
+@unless($investors->isEmpty())
 @section('registrations')
     <h3 class="mt-5 text-center">Registrierungen</h3>
     @include('bills.registrations')
 @endsection
+@endunless
 
 @section('footer')
     @include('bills.footer')
 @endsection
-
