10f0559452689a6948433adbc0eea4e9c66099ac
Mathias Böttcher <m.boettcher@exporo.com>
2018-10-02 14:53:37 +0200

    added exporo cloud script collection and config

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+FROM registry.gitlab.com/exporo/alpine-nginx-php-fpm:0.0.8
+
+RUN apk -v --update --no-cache add openssh mysql-client zip curl tar && \
+    sed -i s/#PermitRootLogin.*/PermitRootLogin\ without-password/ /etc/ssh/sshd_config
+
+RUN curl -L https://download.newrelic.com/php_agent/archive/8.2.0.221/newrelic-php5-8.2.0.221-linux-musl.tar.gz | tar -C /tmp -zx && \
+    NR_INSTALL_USE_CP_NOT_LN=1 NR_INSTALL_SILENT=1 /tmp/newrelic-php5-*/newrelic-install install && \
+    rm -rf /tmp/newrelic-php5-* /tmp/nrinstall*
+
+COPY ./infrastructure/docker/manifest/ /
+RUN chmod 755 /entrypoint.sh
+ADD ./application/ /var/www/html
+RUN chmod -R 777 /var/www/html/
+
+WORKDIR /var/www/html
+
+EXPOSE 8080
+EXPOSE 22
+
+CMD ["/bin/bash", "/entrypoint.sh"]
---
+++ infrastructure/docker/manifest/entrypoint.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/env bash
+
+ssh-keygen -A
+
+# handle error behavior ba
+if [[ "$ERRORS" != "1" ]] ; then
+  sed -i -e "s/error_reporting =.*=/error_reporting = E_ALL/g" /usr/etc/php.ini
+  sed -i -e "s/display_errors =.*/display_errors = stdout/g" /usr/etc/php.ini
+fi
+
+if [[ -v NO_OPCACHE ]]; then
+    sed -i -e "s/zend_extension=opcache.so/;zend_extension=opcache.so/g" /etc/php.d/zend-opcache.ini
+fi
+
+# assume DEPLOYMENT_ENV is only set in aws cloud, meaning task role is applied and ssm is authorized
+if [[ -n "${DEPLOYMENT_ENV}" ]]; then
+
+  # provide simple/fast way of investigation and manual interaction in containers via ssh, as FARGATE doesn't provide any (docker exec or similar)
+  # TODO review for fast tweaks
+  echo "configure ssh daemon with /app/${DEPLOYMENT_ENV}/ssh-key-pub"
+
+  mkdir -p /root/.ssh
+  aws ssm get-parameter --region "${AWS_REGION}" --name /app/${DEPLOYMENT_ENV}/ssh-key-pub --with-decryption --output text --query "Parameter.Value" > /root/.ssh/authorized_keys
+  chmod 600 /root/.ssh/authorized_keys
+
+
+  # take dot-env file approach for parameter handling readability purposes, see environment var scope and piped sub processes
+  parameterPath=/app/${DEPLOYMENT_ENV}/env
+  echo "write dot-env file from ssm parameters with path ${parameterPath}"
+
+  dotEnvFile=.env
+  # TODO remove existing .env before build - provide default values differently
+  rm -f ${dotEnvFile} && touch ${dotEnvFile}
+
+  aws ssm get-parameters-by-path --region "${AWS_REGION}" --path "${parameterPath}" --with-decryption --query 'Parameters[*].[Name, Value]' --output text | while read -r rawKey envValue; do
+    envKey=${rawKey/$parameterPath\//}
+    echo "reading ${rawKey}, exporting to dot-env file as ${envKey}"
+    if [[ "$envValue" == *\ * ]]; then
+        sanitizedEnvValue='"'${envValue}'"'
+    else
+        sanitizedEnvValue=${envValue}
+    fi
+    echo ${envKey}="${sanitizedEnvValue}" >> ${dotEnvFile}
+  done
+
+  chmod 755 ${dotEnvFile}
+
+  export $(cat ${dotEnvFile} | xargs)
+
+  sed -i -e 's/"REPLACE_WITH_REAL_KEY"/${NEW_RELIC_LICENSE_KEY}/' \
+     -e 's/newrelic.appname = "PHP Application"/newrelic.appname = "${OPS_APP_NAME}_${DEPLOYMENT_ENV}"/' \
+     -e 's/;newrelic.enabled = true/newrelic.enabled = true/' \
+        /etc/php.d/newrelic.ini
+
+    ln -s /usr/lib/php/extensions/no-debug-non-zts-20160303/newrelic.so /usr/lib/php/modules/newrelic.so
+else
+  echo "no aws region and deployment param detected - no sshd configured"
+fi
+
+php artisan migrate --force
+
+# super simple process handling, esp restarting on a crash
+/usr/bin/supervisord -n -c /etc/supervisord.conf
