3ce629b21dbdf6596ddbabe0a75ad8b71454ccbc
André Gräßel <hello@spaceemotion.net>
2018-11-02 18:25:10 +0100

    Update package dependencies

--- application/resources/js/icons.js
+++ application/resources/js/icons.js
@@ -1,15 +1,15 @@
 import fontawesome from '@fortawesome/fontawesome';
 
 fontawesome.library.add(
-  require('@fortawesome/fontawesome-free-solid/faChevronDown'),
-  require('@fortawesome/fontawesome-free-solid/faChevronRight'),
-  require('@fortawesome/fontawesome-free-solid/faFlag'),
-  require('@fortawesome/fontawesome-free-solid/faHome'),
-  require('@fortawesome/fontawesome-free-solid/faShareSquare'),
-  require('@fortawesome/fontawesome-free-solid/faSort'),
-  require('@fortawesome/fontawesome-free-solid/faSortDown'),
-  require('@fortawesome/fontawesome-free-solid/faSortUp'),
-  require('@fortawesome/fontawesome-free-solid/faSync'),
-  require('@fortawesome/fontawesome-free-solid/faUser'),
-  require('@fortawesome/fontawesome-free-solid/faUsers'),
+  require('@fortawesome/free-solid-svg-icons/faChevronDown').definition,
+  require('@fortawesome/free-solid-svg-icons/faChevronRight').definition,
+  require('@fortawesome/free-solid-svg-icons/faFlag').definition,
+  require('@fortawesome/free-solid-svg-icons/faHome').definition,
+  require('@fortawesome/free-solid-svg-icons/faShareSquare').definition,
+  require('@fortawesome/free-solid-svg-icons/faSort').definition,
+  require('@fortawesome/free-solid-svg-icons/faSortDown').definition,
+  require('@fortawesome/free-solid-svg-icons/faSortUp').definition,
+  require('@fortawesome/free-solid-svg-icons/faSync').definition,
+  require('@fortawesome/free-solid-svg-icons/faUser').definition,
+  require('@fortawesome/free-solid-svg-icons/faUsers').definition,
 );
