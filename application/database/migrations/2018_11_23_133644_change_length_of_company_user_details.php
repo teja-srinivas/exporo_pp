a8be8a732601e1c8bd49b8e095c9a17cfd857028
AVertgewall <a.vertgewall@exporo.com>
2018-11-26 14:36:14 +0100

    added migration for username/company and fixed wrong class naming in Migration

--- application/database/migrations/2018_11_23_133644_change_length_of_company_user_details.php
+++ application/database/migrations/2018_11_23_133644_change_length_of_company_user_details.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class ChangeLengthOfBic extends Migration
+class ChangeLengthOfCompanyUserDetails extends Migration
 {
     /**
      * Run the migrations.
