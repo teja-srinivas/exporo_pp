56de90c0cdbb82f5ba45af39688eb3a65787c57e
André Gräßel <hello@spaceemotion.net>
2018-11-30 16:38:44 +0100

    Add custom api token support using a token service

---
+++ application/app/Services/ApiTokenService.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Services;
+
+use Illuminate\Support\Facades\Hash;
+
+/**
+ * Service that creates unique api tokens based on a service identifier
+ * and customizable parameters for are unique for e.g. a request.
+ */
+class ApiTokenService
+{
+    /**
+     * Creates an api token for the given service with the given parameters.
+     *
+     * @param string $service
+     * @param mixed ...$params
+     * @return string
+     */
+    public function forService(string $service, ...$params): string
+    {
+        return base64_encode(Hash::make($this->getIdentifier($service, $params)));
+    }
+
+    /**
+     * Checks if the given "token" is valid for our service and parameter settings.
+     *
+     * @param string $service
+     * @param string $token
+     * @param mixed ...$params
+     * @return bool
+     */
+    public function isValid(string $service, string $token, ... $params): bool
+    {
+        return Hash::check($this->getIdentifier($service, $params), base64_decode($token));
+    }
+
+    /**
+     * @param string $service
+     * @param array $params
+     * @return string
+     */
+    private function getIdentifier(string $service, array $params): string
+    {
+        return $service . '.' . implode('-', $params);
+    }
+}
---
+++ application/tests/Unit/ApiTokenServiceTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Services\ApiTokenService;
+use Tests\TestCase;
+
+class ApiTokenServiceTest extends TestCase
+{
+    /** @test */
+    public function it_creates_custom_service_tokens()
+    {
+        /** @var ApiTokenService $service */
+        $service = $this->app->make(ApiTokenService::class);
+        $app = 'phpunit';
+
+        $this->assertTrue($service->isValid($app, $service->forService($app)));
+    }
+}
