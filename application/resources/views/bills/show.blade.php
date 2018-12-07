0d196dceaa545844bc26ae926430b2d52b461606
André Gräßel <hello@spaceemotion.net>
2018-12-07 15:41:17 +0100

    Use the same inclusion logic for bill partials everywhere

--- application/resources/views/bills/show.blade.php
+++ application/resources/views/bills/show.blade.php
@@ -9,6 +9,8 @@
 @endsection
 
 @section('main-content')
-    @include('bills.pdf.projects')
-    @include('bills.pdf.registrations')
+    @includeWhen($investments->isNotEmpty(), 'bills.pdf.projects')
+    @includeWhen($investors->isNotEmpty(), 'bills.pdf.registrations')
+    @includeWhen($overheads->isNotEmpty(), 'bills.pdf.overhead')
+    @includeWhen($custom->isNotEmpty(), 'bills.pdf.custom')
 @endsection
