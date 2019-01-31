3e0c880bc416816f97419da809767f799fab4d4c
André Gräßel <hello@spaceemotion.net>
2019-01-31 10:01:36 +0100

    Add link to Nova panel for admins

--- application/resources/views/layouts/sidebar.blade.php
+++ application/resources/views/layouts/sidebar.blade.php
@@ -6,6 +6,12 @@
             {{-- Sidebar --}}
             <div class="col-md-3 col-xl-2">
                 <div class="sidebar sticky-top mb-3">
+                    @role('admin')
+                        <a href="{{ config('nova.path') }}" class="btn btn-outline-primary btn-block mb-3">
+                            Admin-Panel
+                        </a>
+                    @endrole
+
                     @foreach($menu as $group)
                         <h6 class="text-muted text-uppercase tracking-wide sidebar-header @unless($loop->first) mt-4 @endif">
                             {{ $group['title'] }}
