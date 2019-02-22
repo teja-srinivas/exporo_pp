040248575a51434563034878bd950ad47dc0eddf
André Gräßel <hello@spaceemotion.net>
2019-02-22 14:46:36 +0100

    Add simple Home page feature test

---
+++ application/tests/Feature/Pages/HomeTest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Tests\Feature\Pages;
+
+use App\Models\Bill;
+use App\Models\User;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Tests\TestCase;
+
+class HomeTest extends TestCase
+{
+    use DatabaseMigrations;
+    use WithoutMiddleware;
+
+    /** @var User */
+    private $user;
+
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->user = factory(User::class)->create();
+    }
+
+    /** @test */
+    public function it_shows_latest_bills()
+    {
+        $bill = $this->createBill(['released_at' => '2018-11-01']);
+        $billWithoutPdf = $this->createBill(['released_at' => '2018-12-01', 'pdf_created' => false]);
+
+        $this->be($this->user);
+
+        $response = $this->get(route('home'));
+
+        $response->assertOk();
+
+        $response->assertSee($bill->getDisplayName());
+        $response->assertDontSee($billWithoutPdf->getDisplayName());
+    }
+
+    protected function createBill(array $attributes = []): Bill
+    {
+        /** @var Bill $bill */
+        $bill = factory(Bill::class)->state('released')->create($attributes + [
+            'user_id' => $this->user->getKey(),
+        ]);
+
+        $bill->commissions()->create([
+            'user_id' => $this->user->getKey(),
+            'net' => 10,
+            'gross' => 10,
+        ]);
+
+        return $bill;
+    }
+}
