b9f8cad47c00dade287cc10f3ebcea864ece835d
André Gräßel <hello@spaceemotion.net>
2019-02-22 11:53:59 +0100

    Add “released” state to BillFactory

--- application/database/factories/BillFactory.php
+++ application/database/factories/BillFactory.php
@@ -2,8 +2,17 @@
 
 use Faker\Generator as Faker;
 
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
 $factory->define(\App\Models\Bill::class, function (Faker $faker) {
     return [
         'released_at' => $faker->randomDigit > 3 ? $faker->date() : null,
+    ];
+});
+
+$factory->state(\App\Models\Bill::class, 'released', function (Faker $faker) {
+    return [
+        'released_at' => $faker->dateTimeBetween(\App\Models\Commission::LAUNCH_DATE)->format('Y-m-d'),
+        'pdf_created' => true,
     ];
 });
