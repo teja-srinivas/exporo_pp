fd8f8a36bb96ef082e776bdca0a7cbf16e789cac
André Gräßel <hello@spaceemotion.net>
2019-03-18 13:39:06 +0100

    Add PHPDocs to company model

--- application/app/Models/Company.php
+++ application/app/Models/Company.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Cog\Laravel\Optimus\Traits\OptimusEncodedRouteKey;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
@@ -10,7 +11,20 @@ use OwenIt\Auditing\Auditable;
 use OwenIt\Auditing\Contracts\Auditable as AuditableContract;
 
 /**
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @property string $name
+ * @property string|null $city
+ * @property string|null $email
+ * @property string|null $fax_number
+ * @property string|null $phone_number
+ * @property string|null $postal_code
+ * @property string|null $street
+ * @property string|null $street_no
+ * @property string|null $website
+ *
  * @property Collection $bannerSets
+ * @property Collection $users
  */
 class Company extends Model implements AuditableContract
 {
