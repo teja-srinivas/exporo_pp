2ed5e78039d9b0f93c0e8b56649066768ce46f59
André Gräßel <hello@spaceemotion.net>
2019-03-18 17:51:01 +0100

    Add defaults to contract templates table

--- application/app/Models/ContractTemplate.php
+++ application/app/Models/ContractTemplate.php
@@ -9,7 +9,12 @@ use Illuminate\Support\Collection;
 
 /**
  * @property int $id
+ * @property string $name
  * @property string|null $body
+ * @property int $cancellation_days
+ * @property int $claim_years
+ * @property bool $vat_included
+ * @property float $vat_amount
  * @property Carbon $created_at
  * @property Carbon $updated_at
  *
@@ -17,7 +22,16 @@ use Illuminate\Support\Collection;
  */
 class ContractTemplate extends Model
 {
-    protected $fillable = ['body'];
+    protected $casts = [
+        'cancellation_days' => 'int',
+        'claim_years' => 'int',
+        'vat_included' => 'bool',
+        'vat_amount' => 'float',
+    ];
+
+    protected $fillable = [
+        'body', 'name', 'cancellation_days', 'claim_years', 'vat_included', 'vat_amount',
+    ];
 
     public function contracts(): HasMany
     {
