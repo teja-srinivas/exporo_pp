5cc0bc195164c567bdc318d4fcb437b0ceda005a
André Gräßel <hello@spaceemotion.net>
2019-04-01 11:00:04 +0200

    Make BonusBundle->child_user_selectable editable
    as well as show it on the index screen

--- application/resources/views/commissions/bundles/edit.blade.php
+++ application/resources/views/commissions/bundles/edit.blade.php
@@ -41,10 +41,18 @@
                 ],
                 [
                     'type' => 'checkbox',
-                    'label' => __('Typ'),
+                    'label' => __('Auswahl'),
                     'name' => 'selectable',
                     'description' => 'Für Partner zur Auswahl möglich',
                     'default' => $bundle->selectable,
+                ],
+                [
+                    'type' => 'checkbox',
+                    'label' => __('Subpartner'),
+                    'name' => 'child_user_selectable',
+                    'description' => 'Für Sub-Partner zur Auswahl möglich',
+                    'default' => true,
+                    'help' => 'Partner, die geworben wurden und somit eine Subpartner-Beziehung haben, bekommen eine andere Auswahl.'
                 ],
             ]
         ])
