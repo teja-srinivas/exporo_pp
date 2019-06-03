5ac48175899f6d16b34470f48be2315f428fb639
André Gräßel <hello@spaceemotion.net>
2019-06-03 11:28:25 +0200

    Add “SnipeMigrations” to speed up local tests

--- application/tests/TestCase.php
+++ application/tests/TestCase.php
@@ -2,9 +2,10 @@
 
 namespace Tests;
 
+use Drfraker\SnipeMigrations\SnipeMigrations;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    use CreatesApplication;
+    use CreatesApplication, SnipeMigrations;
 }
