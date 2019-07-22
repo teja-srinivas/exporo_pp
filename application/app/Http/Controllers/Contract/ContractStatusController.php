2ce5f786990579785d68e4607342362ce747c28f
André Gräßel <hello@spaceemotion.net>
2019-07-22 18:16:40 +0200

    Fix updating a contract’s status

--- application/app/Http/Controllers/Contract/ContractStatusController.php
+++ application/app/Http/Controllers/Contract/ContractStatusController.php
@@ -23,12 +23,20 @@ class ContractStatusController extends Controller
         $this->authorize('update', $contract);
 
         $this->checkIfContractIsEditable($contract);
-dd($request, $contract);
-        $contract->update([
-            'released_at' => now(),
+
+        $data = $this->validate($request, [
+            'release' => 'required|boolean',
         ]);
 
-        flash_success('Vertrag wurde dem Partner zur Unterschrift freigegeben.');
+        if ($data['release']) {
+            $now = now();
+            $contract->forceFill(['released_at' => $now, 'accepted_at' => $now])->save();
+            $contract->user->forceFill(['accepted_at' => $now])->save();
+            flash_success('Vertrag wurde freigegeben. Der Benutzer ist nun aktiviert.');
+        } else {
+            $contract->update(['released_at' => null]);
+            flash_success('Vertrag wurde wieder in den Entwurfsstatus gestellt.');
+        }
 
         return back();
     }
