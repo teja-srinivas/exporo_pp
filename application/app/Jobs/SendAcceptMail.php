0316bf86e2b0245d675af15daa1e7ca08f7cde18
André Gräßel <hello@spaceemotion.net>
2019-07-29 15:22:29 +0200

    Don’t queue proxy sendmail jobs

--- application/app/Jobs/SendAcceptMail.php
+++ application/app/Jobs/SendAcceptMail.php
@@ -10,7 +10,7 @@ use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 
-class SendAcceptMail implements ShouldQueue
+class SendAcceptMail
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
--- application/app/Jobs/SendRejectMail.php
+++ application/app/Jobs/SendRejectMail.php
@@ -9,7 +9,7 @@ use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 
-class SendRejectMail implements ShouldQueue
+class SendRejectMail
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
