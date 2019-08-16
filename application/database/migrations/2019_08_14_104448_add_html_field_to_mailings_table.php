a632b2f1e23a07ef7eb40e7f0d7e4e7800aa4842
André Gräßel <hello@spaceemotion.net>
2019-08-16 11:26:39 +0200

    Rename HTML mailings migration

--- application/database/migrations/2019_08_14_104448_add_html_field_to_mailings_table.php
+++ application/database/migrations/2019_08_14_104448_add_html_field_to_mailings_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class MailingsHtml extends Migration
+class AddHtmlFieldToMailingsTable extends Migration
 {
     /**
      * Run the migrations.
