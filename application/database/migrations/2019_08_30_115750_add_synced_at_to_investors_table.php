85b835c081e68ad13efc9b230028e51f6004713b
André Gräßel <hello@spaceemotion.net>
2019-08-30 14:41:33 +0200

    Add synced_at column to investors

---
+++ application/database/migrations/2019_08_30_115750_add_synced_at_to_investors_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddSyncedAtToInvestorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('investors', function (Blueprint $table) {
+            $table->timestamp('synced_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('investors', function (Blueprint $table) {
+            $table->dropColumn('synced_at');
+        });
+    }
+}
