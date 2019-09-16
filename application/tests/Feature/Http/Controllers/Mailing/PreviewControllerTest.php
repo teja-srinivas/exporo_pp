e9eb0eee3c149984ac31b93c9e3f2f8d14f50725
André Gräßel <hello@spaceemotion.net>
2019-09-16 16:18:41 +0200

    Add Mailing\PreviewControllerTest

---
+++ application/tests/Feature/Http/Controllers/Mailing/PreviewControllerTest.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Feature\Http\Controllers\Mailing;
+
+use Tests\TestCase;
+use App\Models\Mailing;
+use Tests\TestsControllers;
+use App\Policies\MailingPolicy;
+
+class PreviewControllerTest extends TestCase
+{
+    use TestsControllers;
+
+    /**
+     * @test
+     */
+    public function it_replaces_variables_for_mail_previews()
+    {
+        $user = $this->createActiveUserWithPermission(MailingPolicy::PERMISSION.'.view');
+
+        $this->be($user);
+
+        $mailing = factory(Mailing::class)->create([
+            'html' => '<b>ID %partnerid%</b>, Your %partnername% (<a href="//track.me/%reflink%"/>click here</a>',
+        ]);
+
+        $response = $this->get(route('affiliate.mails.preview', $mailing));
+        $expected = '<b>ID '.$user->getKey().'</b>, Your '.$user->first_name.' '.$user->last_name
+            .' (<a href="//track.me/?a_aid='.$user->getKey().'"/>click here</a>';
+
+        $this->assertEquals($expected, $response->content());
+    }
+
+}
