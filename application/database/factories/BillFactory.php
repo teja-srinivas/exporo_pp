5dd1e7a671449685f270bc8c834151e20960184f
André Gräßel <hello@spaceemotion.net>
2019-09-23 12:40:45 +0200

    Clean up database BillFactory

--- application/database/factories/BillFactory.php
+++ application/database/factories/BillFactory.php
@@ -1,18 +1,21 @@
 <?php
 
+use App\Models\Bill;
+use App\Models\Commission;
 use Faker\Generator as Faker;
+use Illuminate\Database\Eloquent\Factory;
 
-/* @var \Illuminate\Database\Eloquent\Factory $factory */
+/* @var Factory $factory */
 
-$factory->define(\App\Models\Bill::class, static function (Faker $faker) {
+$factory->define(Bill::class, static function (Faker $faker) {
     return [
         'released_at' => $faker->randomDigit > 3 ? $faker->date() : null,
     ];
 });
 
-$factory->state(\App\Models\Bill::class, 'released', static function (Faker $faker) {
+$factory->state(Bill::class, 'released', static function (Faker $faker) {
     return [
-        'released_at' => $faker->dateTimeBetween(\App\Models\Commission::LAUNCH_DATE)->format('Y-m-d'),
-        'pdf_created_at' => $faker->dateTimeBetween(\App\Models\Commission::LAUNCH_DATE)->format('Y-m-d'),
+        'released_at' => $faker->dateTimeBetween(Commission::LAUNCH_DATE)->format('Y-m-d'),
+        'pdf_created_at' => $faker->dateTimeBetween(Commission::LAUNCH_DATE)->format('Y-m-d'),
     ];
 });
