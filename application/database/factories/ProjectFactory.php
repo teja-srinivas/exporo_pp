8f7bd738fa8446f74a14944f618f4fc51eef0565
André Gräßel <hello@spaceemotion.net>
2019-09-23 14:36:32 +0200

    Update project database factory with better defaults

--- application/database/factories/ProjectFactory.php
+++ application/database/factories/ProjectFactory.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Factory;
 /* @var Factory $factory */
 
 $factory->define(Project::class, static function (Faker $faker) {
-    $paybackMin = $faker->date();
+    $paybackMin = $faker->dateTimeBetween('now', '+4 years');
 
     return [
         'id' => $faker->unique()->numberBetween(),
@@ -16,8 +16,8 @@ $factory->define(Project::class, static function (Faker $faker) {
         'interest_rate' => $faker->randomFloat(2, 1, 4),
         'margin' => $faker->randomFloat(2, 1, 3),
         'image' => $faker->imageUrl(800, 600, 'city'),
-        'description' => $faker->text,
-        'launched_at' => $faker->randomDigit > 6 ? $faker->date() : null,
+        'description' => $faker->streetAddress,
+        'launched_at' => $faker->randomDigit > 6 ? $faker->dateTimeBetween('-4 years') : null,
         'payback_min_at' => $paybackMin,
         'payback_max_at' => Carbon::parse($paybackMin)->addWeeks(2),
     ];
