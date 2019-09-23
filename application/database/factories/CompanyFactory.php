1195eb099507f08677310ae548a3a662a983e105
André Gräßel <hello@spaceemotion.net>
2019-09-23 12:40:34 +0200

    Update company factory with new fields

--- application/database/factories/CompanyFactory.php
+++ application/database/factories/CompanyFactory.php
@@ -1,9 +1,21 @@
 <?php
 
+use App\Models\Company;
 use Faker\Generator as Faker;
+use Illuminate\Database\Eloquent\Factory;
 
-$factory->define(\App\Models\Company::class, static function (Faker $faker) {
+/* @var Factory $factory */
+
+$factory->define(Company::class, static function (Faker $faker) {
     return [
         'name' => $faker->company,
+        'city' => $faker->city,
+        'email' => $faker->email,
+        'fax_number' => $faker->phoneNumber,
+        'phone_number' => $faker->phoneNumber,
+        'postal_code' => $faker->postcode,
+        'street' => $faker->streetName,
+        'street_no' => $faker->streetSuffix,
+        'website' => $faker->url,
     ];
 });
