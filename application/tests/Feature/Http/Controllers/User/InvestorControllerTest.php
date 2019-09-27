e51fa399a0e81c5a76e83326d27d5a1093d214dd
André Gräßel <hello@spaceemotion.net>
2019-09-27 15:06:08 +0200

    Add user investor controller/page test

---
+++ application/tests/Feature/Http/Controllers/User/InvestorControllerTest.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Feature\Http\Controllers\User;
+
+use Tests\TestCase;
+use App\Models\User;
+use App\Models\Investor;
+use App\Models\Investment;
+
+class InvestorControllerTest extends TestCase
+{
+    /** @test */
+    public function it_lists_all_investors_with_investment_aggregation()
+    {
+        /** @var User $user */
+        $user = factory(User::class)->state('accepted')->create();
+
+        /** @var Investor $investor */
+        $investor = factory(Investor::class)->create([
+            'user_id' => $user->getKey(),
+        ]);
+
+        /** @var Investment $paid */
+        $paid = $investor->investments()->create(factory(Investment::class)->state('paid')->raw());
+
+        $response = $this->be($user)->get("/users/{$user->getKey()}/investors");
+        $response->assertOk();
+
+        // Make sure all the details are there
+        $response->assertSee($investor->last_name);
+        $response->assertSee(substr(json_encode([
+            'investments' => 1,
+            'amount' => $paid->amount,
+        ]), 1, -1));
+    }
+}
