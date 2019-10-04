6623d14ebdafb3dbeeee027777d65d5bed74b364
André Gräßel <hello@spaceemotion.net>
2019-10-04 16:35:29 +0200

    Fix casing typo in tag help component

--- application/resources/views/components/tag-help.blade.php
+++ application/resources/views/components/tag-help.blade.php
@@ -1,4 +1,4 @@
-Folgende Textbausteine stehen zur verfügung:<br>
+Folgende Textbausteine stehen zur Verfügung:<br>
 @foreach($tags as $name => $value)
     <span class="badge badge-secondary">{{ $name }}</span> <span class="badge badge-light">{{ value($value) }}</span><br>
 @endforeach
