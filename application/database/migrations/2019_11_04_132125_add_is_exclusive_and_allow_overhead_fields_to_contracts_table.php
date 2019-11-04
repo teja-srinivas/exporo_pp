661811656f4088369c32023762949635c9ae8c24
André Gräßel <hello@spaceemotion.net>
2019-11-04 15:08:21 +0100

    Fix style issues

--- application/database/migrations/2019_11_04_132125_add_is_exclusive_and_allow_overhead_fields_to_contracts_table.php
+++ application/database/migrations/2019_11_04_132125_add_is_exclusive_and_allow_overhead_fields_to_contracts_table.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 use App\Models\Role;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,12 +15,12 @@ class AddIsExclusiveAndAllowOverheadFieldsToContractsTable extends Migration
      */
     public function up()
     {
-        Schema::table('contracts', function (Blueprint $table) {
+        Schema::table('contracts', static function (Blueprint $table) {
             $table->boolean('is_exclusive')->default(false)->after('vat_amount');
             $table->boolean('allow_overhead')->default(false)->after('is_exclusive');
         });
 
-        Schema::table('contract_templates', function (Blueprint $table) {
+        Schema::table('contract_templates', static function (Blueprint $table) {
             $table->boolean('is_exclusive')->default(false)->after('vat_amount');
             $table->boolean('allow_overhead')->default(false)->after('is_exclusive');
         });
@@ -38,11 +40,11 @@ class AddIsExclusiveAndAllowOverheadFieldsToContractsTable extends Migration
      */
     public function down()
     {
-        Schema::table('contracts', function (Blueprint $table) {
+        Schema::table('contracts', static function (Blueprint $table) {
             $table->dropColumn(['is_exclusive', 'allow_overhead']);
         });
 
-        Schema::table('contract_templates', function (Blueprint $table) {
+        Schema::table('contract_templates', static function (Blueprint $table) {
             $table->dropColumn(['is_exclusive', 'allow_overhead']);
         });
     }
