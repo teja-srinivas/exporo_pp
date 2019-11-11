a7508107e9ee067933671a503c558a782519cc68
André Gräßel <hello@spaceemotion.net>
2019-11-11 11:40:42 +0100

    Add migration to update allow_overhead status
    Sets the allow_overhead flag on all active partner contracts based on the active product contract bonuses.

---
+++ application/database/migrations/2019_11_11_093549_set_allow_overhead_flag_on_partner_contract_based_on_product_contract_bonuses.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+
+class SetAllowOverheadFlagOnPartnerContractBasedOnProductContractBonuses extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        User::query()
+            ->with('partnerContract', 'productContract')
+            ->whereHas('partnerContract')
+            ->whereHas('productContract')
+            ->get()
+            ->each(function (User $user) {
+                $contract = $user->partnerContract;
+                $contract->allow_overhead = $user->productContract->hasOverhead();
+                $contract->save();
+            });
+    }
+}
