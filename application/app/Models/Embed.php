619f6364d97a302cf0dd95c1f18e67f16ab95cd2
Dennis Flaig <dennis.flaig@gmx.de>
2019-11-11 16:57:15 +0100

    Fix embed model

---
+++ application/app/Models/Embed.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Embed extends Model
+{
+    /**
+     * @return BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
