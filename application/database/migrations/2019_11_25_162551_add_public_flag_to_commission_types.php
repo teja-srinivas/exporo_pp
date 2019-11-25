d05d2116518cbe35667b2ee7c52d26b6e7c5b3f8
André Gräßel <hello@spaceemotion.net>
2019-11-25 21:48:47 +0100

    Fix linting errors

--- application/database/migrations/2019_11_25_162551_add_public_flag_to_commission_types.php
+++ application/database/migrations/2019_11_25_162551_add_public_flag_to_commission_types.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,7 +15,7 @@ class AddPublicFlagToCommissionTypes extends Migration
      */
     public function up()
     {
-        Schema::table('commission_types', function (Blueprint $table) {
+        Schema::table('commission_types', static function (Blueprint $table) {
             $table->boolean('is_public')->after('is_project_type');
         });
     }
@@ -25,7 +27,7 @@ class AddPublicFlagToCommissionTypes extends Migration
      */
     public function down()
     {
-        Schema::table('commission_types', function (Blueprint $table) {
+        Schema::table('commission_types', static function (Blueprint $table) {
             $table->dropColumn('is_public');
         });
     }
