295459127f4e1c65baa51fa66847a9be67ef5bc2
Dennis Flaig <dennis.flaig@gmx.de>
2019-12-26 09:52:46 +0100

    Add dashboard

--- application/app/Http/Controllers/HomeController.php
+++ application/app/Http/Controllers/HomeController.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
-use Carbon\Carbon;
+use App\Models\Bill;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Collection;
@@ -15,10 +15,35 @@ class HomeController extends Controller
     /**
      * Show the application dashboard.
      *
+     * @param Request $request
+     * @param BillRepository $billRepository
      * @return \Illuminate\Http\Response
      */
-    public function __invoke()
+    public function __invoke(Request $request, BillRepository $billRepository)
     {
-        return response()->view('dashboard.index');
+        /** @var User $user */
+        $user = $request->user();
+
+        /** @var Collection $bills */
+        $bills = $billRepository->getDetails($user->id)
+            ->released()
+            ->visible()
+            ->latest('bills.created_at')
+            ->get();
+
+        return response()->view('home', [
+            'bills' => $bills->map(static function (Bill $bill) {
+                $date = optional($bill->released_at)->format('Y-m-d');
+
+                return [
+                    'displayName' => $bill->getDisplayName(),
+                    'name' => $date,
+                    'date' => $date,
+                    'links' => [
+                        'download' => route('bills.show', $bill),
+                    ],
+                ];
+            }),
+        ]);
     }
 }
