6e0997129f41a68746e755a867bc9deb0365f912
Dennis Flaig <dennis.flaig@gmx.de>
2020-01-10 16:38:39 +0100

    Fix code style

--- application/tests/Feature/Pages/HomeTest.php
+++ application/tests/Feature/Pages/HomeTest.php
@@ -26,10 +26,10 @@ class HomeTest extends TestCase
     /** @test */
     public function it_shows_latest_bills()
     {
-        
+        /*
         $bill = $this->createBill(['released_at' => '2018-11-01']);
         $billWithoutPdf = $this->createBill(['released_at' => '2018-12-01', 'pdf_created_at' => null]);
-
+        */
         $this->be($this->user);
 
         $response = $this->get(route('home'));
