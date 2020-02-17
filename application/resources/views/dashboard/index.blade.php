c076d2f75b80ad50286728e42a7c7e897592ea1d
Dennis Flaig <dennis.flaig@gmx.de>
2020-02-17 17:21:50 +0100

    Add message for users without investments

--- application/resources/views/dashboard/index.blade.php
+++ application/resources/views/dashboard/index.blade.php
@@ -3,9 +3,17 @@
 @section('title', 'Übersicht')
 
 @section('main-content')
-    @php($vueData = [
-        'apiInvestments' => route('api.dashboard.investments'),
-        'apiCommissions' => route('api.dashboard.commissions'),
-    ])
-    <vue v-cloak class="cloak-fade" data-is="investments-viewer" data-props='@json($vueData)' />
+    @if($investmentCount > 0)
+        @php($vueData = [
+            'apiInvestments' => route('api.dashboard.investments'),
+            'apiCommissions' => route('api.dashboard.commissions'),
+        ])
+        <vue v-cloak class="cloak-fade" data-is="investments-viewer" data-props='@json($vueData)' />
+    @else
+        @card
+        <div class="lead text-center text-muted">
+            Das Partner Cockpit ist die zentrale Anlaufstelle für Ihre Tätigkeit als Partner. Sie können hier alle Aktivitäten Ihrer Kunden und Interessenten sehen und erhalten Statistiken zum Verhalten Ihrer Kunden. Zudem stehen Ihnen hier die monatliche Abrechnungen und Provisionsansprüche zur Verfügung. Das Partner Cockpit bieten Ihnen auch auch einen direkten Zugang zu Werbemitteln und Tools für eine nahtlose digitale Integration.
+        </div>
+        @endcard
+    @endif
 @endsection
