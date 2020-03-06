ed0386d6d60eba49149a09d21bf3d64dbf62482c
Dennis Flaig <dennis.flaig@gmx.de>
2020-03-06 17:38:45 +0100

    Revert change

--- application/resources/views/contracts/pdf/product.blade.php
+++ application/resources/views/contracts/pdf/product.blade.php
@@ -134,10 +134,11 @@
     <h3><strong>3. Umsatzsteuer</strong></h3>
     <p style="margin-left: 2rem;">
         Die Provisionen werden von Exporo umsatzsteuerfrei in Rechnung gestellt.
-        @if($contract->vat_amount > 0)
+        @if($contract->vat_amount )
             Sollte der Partner gegenüber Exporo anzeigen, dass seine
             Tätigkeit der Umsatzsteuerpflicht unterliegt, wird Exporo diese in der 
-            Abrechnung gesondert ausweisen.
+            Abrechnung gesondert ausweisen/ wird die Provision inklusive der
+            Umsatzsteuer ausgewiesen.
         @endif
     </p>
 
