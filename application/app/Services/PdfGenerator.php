49a05e601ab01afac6a5efd2ae472ff2d7c19c5b
Dennis Flaig <dennis.flaig@gmx.de>
2020-03-09 09:05:41 +0100

    Change pdf generator

--- application/app/Services/PdfGenerator.php
+++ application/app/Services/PdfGenerator.php
@@ -6,6 +6,7 @@ namespace App\Services;
 
 use DocRaptor\Doc;
 use DocRaptor\DocApi;
+use DocRaptor\Configuration;
 use Illuminate\Support\Str;
 use DocRaptor\PrinceOptions;
 use Illuminate\Http\Request;
@@ -21,13 +22,14 @@ class PdfGenerator
     protected $api;
 
     /** @var bool */
-    protected $test;
+    protected $test = true;
+
+    protected $apiKey = "YOUR_API_KEY_HERE";
 
     public function __construct(Application $app, DocApi $api, Repository $config)
     {
         $this->app = $app;
         $this->api = $api;
-        $this->test = $config->get('services.docraptor.test');
     }
 
     /**
@@ -40,6 +42,9 @@ class PdfGenerator
     {
         // Run an internal request to generate the raw HTML content
         $response = $this->app->handle(Request::create($route));
+
+        $configuration = Configuration::getDefaultConfiguration();
+        $configuration->setUsername($this->apiKey);
 
         // Send the request to DocRaptor - including the content
         $prince = new PrinceOptions();
