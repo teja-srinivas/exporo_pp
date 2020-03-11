8a474de80db7ff86aaeb7e707afa06b720f84fdf
Dennis Flaig <dennis.flaig@gmx.de>
2020-03-11 15:10:09 +0100

    Fix bonues

--- application/app/Http/Controllers/Contract/ContractPdfController.php
+++ application/app/Http/Controllers/Contract/ContractPdfController.php
@@ -13,9 +13,14 @@ class ContractPdfController extends Controller
 {
     public function show(Contract $contract): Response
     {
-        $bonuses = $contract->bonuses->sortBy(static function ($bonus) {
-            return $bonus->type_id;
-        });
+        $bonuses = null;
+
+        if ($contract->bonuses !== null) {
+            $bonuses = $contract->bonuses->sortBy(static function ($bonus) {
+                return $bonus->type_id;
+            });
+        }
+
         $view = "contracts.pdf.{$contract->type}";
 
         abort_unless(View::exists($view), Response::HTTP_NOT_FOUND);
