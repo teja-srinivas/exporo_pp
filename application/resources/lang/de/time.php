a18cd5c190ebd32d952bc87e37f5ab50e9bcc85e
David Jung <d.jung@exporo.com>
2020-11-10 19:48:19 +0100

    added monthname translation because setLocal in Blade not working.

--- application/resources/lang/de/time.php
+++ application/resources/lang/de/time.php
@@ -3,4 +3,18 @@
 return [
     'days' => '[1] :count Tag |[0,2,*] :count Tage',
     'years' => '[1] :count Jahr |[0,2,*] :count Jahre',
+    'monthname' => [
+        '01' => 'Januar',
+        '02' => 'Februar',
+        '03' => 'März',
+        '04' => 'April',
+        '05' => 'Mai',
+        '06' => 'Juni',
+        '07' => 'Juli',
+        '08' => 'August',
+        '09' => 'September',
+        '10' => 'Oktober',
+        '11' => 'November',
+        '12' => 'Dezember',
+    ],
 ];
--- application/resources/views/bills/pdf/cover.blade.php
+++ application/resources/views/bills/pdf/cover.blade.php
@@ -72,7 +72,7 @@
 </div>
 
 <!-- Page Content -->
-<h4 class="mb-4">Provisionsgutschrift {{ optional($bill->released_at->subMonth(1))->format('F Y') }}</h4>
+<h4 class="mb-4">Provisionsgutschrift {{ __('time.monthname.' . $bill->released_at->subMonth(1)->formatLocalized('%m')) }} {{ $bill->released_at->subMonth(1)->formatLocalized('%Y') }}</h4>
 <h5 class="mb-5">Gemäß Partnervertrag mit der Exporo Investment GmbH vom {{ $user->partnerContract->accepted_at->format('d.m.Y') }}</h5>
 
 <p>
