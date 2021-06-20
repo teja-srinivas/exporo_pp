9c39b6e1408847921039bc0641e7c0b1dad88a7e
David Jung <d.jung@exporo.com>
2021-06-20 21:59:31 +0200

    [Asana-1200404556084332] new "Provisionsvereinbarung" Template

--- application/resources/views/contracts/pdf/product.blade.php
+++ application/resources/views/contracts/pdf/product.blade.php
@@ -4,144 +4,125 @@
     <h3><strong>1. Finanzprodukte</strong></h3>
     <div style="margin-left: 2rem;">
         <p>
-            a. Exporo bzw. Unternehmen aus der Exporo Unternehmensgruppe bieten 
-            verschiedene Finanzprodukte an. Hierbei wird grundsätzlich zwischen
-            zwei Produktfamilien unterschieden:
+            <strong>a.	Öffentliche / Nicht öffentliche Angebote</strong>
         </p>
-        <table class="firstTable">
-                <colgroup>
-                    <col style="width: 50%;" />
-                    <col style="width: 50%;" />
-                </colgroup>
-                <tbody>
-                <tr>
-                    <td style="text-align: left;">
-                        <strong>Öffentliche Angebote*</strong>
-                    </td>
-                    <td style="text-align: left;">
-                        <strong>Nicht öffentliche Angebote*</strong>
-                    </td>
-                </tr>
-                <tr>
-                    <td style="text-align: left;">
-                        Hierzu zählen alle Investmentangebote, die auf
-                        <a href="http://exporo.de/">https://exporo.de</a>
-                        für jeden einsehbar und somit abschließbar sind. Aktuell
-                        wird grundsätzlich zwischen zwei unterschiedlichen
-                        Produkten unterschieden:
-
-                        <ul>
-                            <li>Exporo Finanzierung</li>
-                            <li>Exporo Bestand</li>
-                        </ul>
-                    </td>
-                    <td style="text-align: left;">
-                        Hierunter fallen alle Investmentangebote,
-                        die von Exporo als sog. Private
-                        Placements oder PP bezeichnet werden.
-                        Die Einordnung erfolgt im Ermessen von
-                        Exporo.
-                    </td>
-                </tr>
-                </tbody>
-            </table>
-        <p>
-            * Die Einordnung der Finanzprodukte in öffentliche und nicht
-            öffentliche Angebote wird von Exporo vorgenommen und entspricht
-            ausdrücklich nicht den Definitionen des Art. 2 lit. d EU-Prospekt-VO.
-        </p>
-
+        <div style="margin-left: 2rem;">
+            <p>
+                Exporo bzw. Unternehmen aus der Exporo Unternehmensgruppe bieten verschiedene Finanzprodukte an.
+                Hierbei wird grundsätzlich zwischen zwei Produktfamilien unterschieden:
+            </p>
+            <table class="firstTable">
+                    <colgroup>
+                        <col style="width: 50%;" />
+                        <col style="width: 50%;" />
+                    </colgroup>
+                    <tbody>
+                    <tr>
+                        <td style="text-align: left;">
+                            <strong>Öffentliche Angebote*</strong>
+                        </td>
+                        <td style="text-align: left;">
+                            <strong>Nicht öffentliche Angebote*</strong>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="text-align: left;">
+                            Hierzu zählen alle Investmentangebote, die auf
+                            <a href="https://exporo.de/">https://exporo.de</a>
+                            für jeden einsehbar und somit abschließbar sind.
+                            Aktuell wird grundsätzlich zwischen zwei unterschiedlichen Produkten unterschieden:
+                            <ul>
+                                <li>Exporo Finanzierung</li>
+                                <li>Exporo Bestand</li>
+                            </ul>
+                        </td>
+                        <td style="text-align: left;">
+                            Hierunter fallen alle Investmentangebote, die von Exporo als sog. Private Placements
+                            oder PP bezeichnet werden. Die Einordnung erfolgt im Ermessen von Exporo.
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
+            <p>
+                * Die Einordnung der Finanzprodukte in öffentliche und nicht öffentliche Angebote wird
+                von Exporo vorgenommen und entspricht ausdrücklich nicht den Definitionen des Art. 2 lit. d EU-Prospekt-VO.
+            </p>
+        </div>
         <p>
             <strong>b. Produktdefinitionen </strong>
         </p>
         <div style="margin-left: 2rem;">
+            <p><strong>Exporo Finanzierung: </strong></p>
             <p>
-                <strong>i.</strong> Exporo Finanzierung: Exporo bietet über die Webseite 
-                <a href="http://exporo.de/">https://exporo.de</a> den Zugang und
-                das Netzwerk zu professionellen Immobilienprojekten, in welche
-                Investoren über die Plattform ab 100€ in eine Vermögensanlage
-                nach § 1 des Gesetzes über Vermögensanlagen oder in Anleihen 
-                investieren können. Sowohl bei den Vermögensanlagen sowie bei
-                den Anleihen erwirbt Exporo oder ein Unternehmen der Exporo 
-                Unternehmensgruppe die Darlehensforderung aus einem zweckgebundenen
-                Darlehen eines Kooperationspartners für ein konkretes 
-                Immobilienprojekt von einem Kreditinstitut. Bei den Vermögensanlagen
-                erwirbt der Investor sodann von Exporo oder einem Unternehmen
-                der Exporo Unternehmensgruppe eine anteilige Darlehensforderung
-                aus dem zweckgebundenen Darlehen. Bei der Anleihe hingegen 
-                refinanziert Exporo oder ein Unternehmen
-                der Exporo Unternehmesgruppe die Darlehensforderung durch die
-                Emission einer Anleihe, die der Investor erwibt. Die
-                Immobilienprojekte sowie der Kooperationspartner werden zuvor 
-                durch Exporo oder Unternehmen der Exporo Unternehmensgruppe 
-                geprüft und gelangen erst auf die Plattform, wenn sie dem 
-                Exporo Maßstab entsprechen. Wie bei einem Bankdarlehen gibt es 
-                eine fest vereinbarte Laufzeit (zwischen 12 und 36 Monaten) und 
-                einen festen Zinssatz (üblicherweise zwischen 4-6% p.a.) 
-                (nachfolgend "<strong>Exporo Finanzierung</strong>").
+                Exporo bietet über die Webseite <a href="https://exporo.de/">https://exporo.de</a> den Zugang und das
+                Netzwerk zu professionellen Immobilienprojekten, in welche Anleger über die
+                Plattform bereits mit geringen Investmentsummen (i. d. R. ab 1, 100 oder 500 EUR in
+                eine Vermögensanlage nach § 1 des Gesetzes über Vermögensanlagen oder in (tokenbasierten)
+                Anleihen investieren können. Sowohl bei den Vermögensanlagen sowie bei den Anleihen erwirbt
+                Exporo oder ein  Unternehmen der Exporo Unternehmensgruppe (i. d. R. eine Zweckgesellschaft,
+                auch Special Purpose Vehicle (SPV) genannt) die Darlehensforderung aus einem zweckgebundenen
+                Darlehen eines Projektentwicklers für ein konkretes Immobilienprojekt von einem Kreditinstitut.
+                Bei den Vermögensanlagen erwirbt der Investor sodann von Exporo oder einem Unternehmen der Exporo
+                Unternehmensgruppe eine anteilige Darlehensforderung aus dem zweckgebundenen Darlehen. Bei der
+                Anleihe hingegen refinanziert Exporo oder ein Unternehmen der Exporo Unternehmensgruppe die
+                Darlehensforderung durch die Emission einer Anleihe, die der Investor erwibt. Die Immobilienprojekte
+                sowie der Projektentwickler werden zuvor durch Exporo oder Unternehmen der Exporo Unternehmensgruppe
+                geprüft und gelangen erst auf die Plattform, wenn sie dem Exporo Maßstab entsprechen. Es gibt es eine
+                fest vereinbarte Lauf zeit (zwischen 12 und 36 Monaten) und einen festen Zinssatz
+                (üblicherweise zwischen 4-6% p.a.) (nachfolgend "<strong>Exporo Finanzierung</strong>").
+            </p>
+            <p><strong>Exporo Bestand: </strong></p>
+            <p>
+                Exporo bietet über die Webseite <a href="https://exporo.de/">https://exporo.de</a>,
+                <a href="https://www.propvest.de/">https://www.propvest.de/</a> bzw. einer anderen
+                von Exporo betriebenen Landingpage im Rahmen des Produktes "Exporo Bestand" bzw. perspektivisch
+                „Propvest“ Bestandsobjekte an, die in der Regel vermietet sind oder vermietet werden sollen. Ein Teil
+                des Kaufpreises wird in der Regel über eine Bank finanziert. Über die Plattform können sich Anleger an
+                diesen Immobilien beteiligen. Die Anleger erhalten eine quartalsweise Ausschüttung der Mietüberschüsse
+                (i.d.R. 4-5% p.a.). Am Ende der Laufzeit (i.d.R. 10 Jahre) wird die erzielte Wertsteigerung, sofern
+                vorhanden, beim Verkauf der Immobilie zu 80% je nach Anzahl der Anteile an die Anleger ausgeschüttet.
+                Exporo übernimmt die gesamte Verwaltung und Weiterentwicklung der Immobilie.
+            </p>
+        </div>
+        <p>
+            <strong>c. Anpassungen und Risikohinweise</strong>
+        </p>
+        <div style="margin-left: 2rem;">
+            <p>
+                Exporo behält sich das Recht vor, die bestehenden Finanzprodukte jederzeit anzupassen oder
+                durch neue zu erweitern. Über Änderungen wird der Partner durch Exporo informiert, indem Exporo einen
+                aktualisierten Anhang "Provisionsvereinbarungen" im Partner Cockpit hinterlegt. Eine Änderung muss nicht
+                dazu führen, dass die Vergütungsvereinbarung insgesamt neu abgeschlossen werden muss.
             </p>
             <p>
-                <strong>ii.</strong> Exporo Bestand: Exporo bietet über die Webseite
-                <a href="http://exporo.de/">https://exporo.de</a> im Rahmen des
-                Produktes "Exporo Bestand" Bestandsobjekte, die in der Regel
-                vermietet sind oder vermietet werden sollen. Ein Teil des
-                Kaufpreises wird in der Regel über eine Bank finanziert. Über
-                die Plattform können sich Investoren in 1.000€-Stücken an diesen
-                Immobilien beteiligen. Die Investoren erhalten eine quartalsweise
-                Ausschüttung der Mietüberschüsse (i.d.R. 4-5% p.a.). Am Ende der
-                Laufzeit (i.d.R. 10 Jahre) wird die erzielte Wertsteigerung,
-                sofern vorhanden, beim Verkauf der Immobilie zu 80% je nach
-                Anzahl der Anteile an die Investoren ausgeschüttet. Exporo
-                übernimmt die gesamte Verwaltung und Weiterentwicklung der
-                Immobilie (nachfolgend "<strong>Exporo Bestand</strong>").
-            </p>
-            <p>
-                <strong>iii.</strong> Exporo behält sich das Recht vor, die bestehenden
-                Finanzprodukte jederzeit anzupassen oder durch neue zu
-                erweitern. Über Änderungen wird der Partner durch Exporo
-                informiert, indem Exporo einen aktualisierten Anhang 
-                "Provisionsvereinbarungen" im Partner Cockpit hinterlegt. 
-                Eine Änderung muss nicht dazu führen, dass die 
-                Vergütungsvereinbarung insgesamt neu abgeschlossen werden muss.
+                Bei den Anlagen besteht das Risiko eines Totalverlustes, z. B. im Falle einer Insolvenz des Projektentwicklers,
+                wenn die Sicherheiten nicht ausreichen, um die Forderungen der Anleger zu bedienen. Aus diesem Grund enthalten
+                die Plattform und die Vertragsdokumente für die Anleger stets einen Hinweis wie z. B. „Der Erwerb dieser
+                Vermögensanlagen und Wertpapiere ist mit erheblichen Risiken verbunden und kann zum vollständigen Verlust
+                des eingesetzten Vermögens führen.“
             </p>
         </div>
     </div>
-    
+
     <h3><strong>2. Höhe der Vergütung </strong></h3>
     <p style="margin-left: 2rem;">
-        a. Ist ein Vergütungsanspruch entstanden, vereinbart Exporo mit dem 
-        Partner hiermit für die Berechnung der Vergütung folgende Beträge:
+        a. Ist ein Vergütungsanspruch entstanden, vereinbart Exporo mit dem Partner hiermit für die Berechnung der Vergütung folgende Beträge:
     </p>
 
     @include('components.bonus-table', [
         'bonuses' => $bonuses,
     ])
 
-    @if($contract->user->contract && $contract->user->contract->allow_overhead)
+    @if(0 && $contract->user->contract && $contract->user->contract->allow_overhead)
     <p style="margin-left: 2rem;">
-        Der Partner, dessen Sub-Partner ein Endgültiges Investment vermittelt 
-        hat, erhält eine Overhead-Partner Provision. Bei der Berechnung der
-        Overhead-Partner Provision ist der für den Overhead-Partner für das 
-        jeweilige Finanzprodukt vereinbarte Prozentsatz heranzuziehen und
-        von diesem Prozentsatz ist die Provision des Sub-Partners zu 
-        subtrahieren. Beispiel: Wurde mit dem Overhead-Partner eine Höhe 
-        von 3,00 % vereinbart und erhält der Sub-Partner für das Investment 
-        2,50 %, so ergibt sich für den Overhead-Partner einen für die Berechnung 
-        der Vergütung heranzuziehenden Prozentsatz von 0,50 %.
+        Der Partner, dessen Sub-Partner ein Endgültiges Investment vermittelt hat, erhält eine Overhead-Partner Provision.
+        Bei der Berechnung der Overhead-Partner Provision ist der für den Overhead-Partner für das jeweilige Finanzprodukt
+        vereinbarte Prozentsatz heranzuziehen und von diesem Prozentsatz ist die Provision des Sub-Partners zu subtrahieren.
+        Beispiel: Wurde mit dem Overhead-Partner eine Höhe von 2,50 % vereinbart und erhält der Sub-Partner für das
+        Investment 3,00 %, so ergibt sich für den Overhead-Partner einen für die Berechnung der Vergütung heranzuziehenden
+        Prozentsatz von 0,50 %.
     </p>
     @endif
-
-    <h3><strong>3. Umsatzsteuer</strong></h3>
-    <p style="margin-left: 2rem;">
-        @if($contract->vat_amount > 0)
-            Sollte der Partner gegenüber Exporo anzeigen, dass seine
-            Tätigkeit der Umsatzsteuerpflicht unterliegt, wird Exporo diese in der 
-            Abrechnung gesondert ausweisen. Die Erhebung erfolgt grundsätzlich für jede
-            Einzelbuchung. Die Darstellung erfolgt aufsummiert.
-        @else
-            Die Provisionen werden von Exporo umsatzsteuerfrei in Rechnung gestellt.
-        @endif
-    </p>
 
     <!-- Scripts -->
     <script src="{{ url(mix('js/manifest.js')) }}"></script>
